b06e6e6453c642a31239360c5c2beaa9f28b6fa2
WinNode <maladarsih@gmail.com>
2025-04-03 11:45:32 +0700

    Update chains.js

--- config/chains.js
+++ config/chains.js
@@ -28,7 +28,7 @@ export const chains = [
     rpcUrl: "https://tea-sepolia.g.alchemy.com/v2/vot1HrCuj9CmekoM2FKNnv6h4Mnzjyb_",
     chainId: "10218",
     symbol: "TEA",
-    explorer: "https://sepolia.tea.xyz/",
+    explorer: "https://sepolia.tea.xyz",
     isMainnet: false
   }
 ];
